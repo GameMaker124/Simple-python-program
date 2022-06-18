a4cb1d13816670e8965765a5b59fad3aa32dd77a
GameMaker124 <107333074+GameMaker124@users.noreply.github.com>
2022-06-17 19:39:37 -0700

    Add files via upload

--- Supper.py
+++ Supper.py
@@ -1,3 +1,22 @@
-print("Welcome To The Super Advance AI")
-name = input("What is your name? ")
-print("Oh Hi" + name "Nice to meet you!")
+AI = "Supper"
+Ai = "SUPPER"
+name = input("Hi My name is Supper I am a AI!! What is your name? ")
+print("Oh Hi " + name + " Nice to meet you!")
+food = input("What is your favorite food? " + name + " ")
+drink = input("What is your favorite drink? ")
+print("WARNING WARNING!!!!!")
+print(".")
+print("..")
+print("...")
+input("  ")
+input(food + " has been made! " + drink + " was also made! ")
+input("Just kidding " + name + " ")
+print("There is no food")
+input("you have been lied to again " + name)
+print("The person who is lying to you is " + AI)
+print("But you may think that you can always trust an AI but you are wrong")
+trusted = input("Wait can you be trusted? ")
+print(trusted + " I see I see")
+input("I don't trust you ")
+
+
